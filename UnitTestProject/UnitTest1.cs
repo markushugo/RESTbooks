9d5a7e2073ba3bb61eaa7437bf4655336379ef09
Markus Viktor Lynggaard <mal024@edu.zealand.dk>
2026-03-20 10:04:25 +0100

    XUnit unittest one with error one that works

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using RESTbooks.Models;
+using RESTbooks.Repos;
+using System.Linq;
+using Xunit;
+
+namespace UnitTestProject
+{
+    public class BookRepoTests
+    {
+        [Fact]
+        public void GetAllBooks_ReturnsAllBooks()
+        {
+            // Arrange
+            var repo = new BookRepo(false);
+            repo.AddBook(new Book { Title = "Test Book 1", Author = "Author 1", Price = 100.0 });
+            repo.AddBook(new Book { Title = "Test Book 2", Author = "Author 2", Price = 200.0 });
+
+            // Act
+            var books = repo.GetAllBooks();
+
+            // Assert
+            Assert.Equal(2, books.Count());
+        }
+
+        [Fact]
+        public void GetBookById_ExistingId_ReturnsBook()
+        {
+            // Arrange
+            var repo = new BookRepo(false);
+            var added = repo.AddBook(new Book { Title = "Test Book", Author = "Author", Price = 50.0 });
+
+            // Act
+            var book = repo.GetBookById(added.Id);
+
+            // Assert
+            Assert.NotNull(book);
+            Assert.Equal("Tests Book", book.Title);
+            Assert.Equal("Author", book.Author);
+            Assert.Equal(50.0, book.Price);
+        }
+    }
+}
